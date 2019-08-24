e18f64a2099e27e8d813126eedac9ed84200cb1c
cheddb <charlesedwin.debrouwer@gmail.com>
2019-08-24 20:08:16 +0200

    player dir

--- player.cpp
+++ player.cpp
@@ -3,10 +3,11 @@
 Player::Player()
 {
     life = 100;
+    sprite = sprite(g_tex.getTexture(TextureManager::Character));
 }
 Player::~Player()
 {
-    
+
 }
 
 
@@ -14,6 +15,13 @@ void Player::frame()
 {
     int oldX = pos.x;
     int oldY = pos.y;
+    float oldDir = dir;
+
+    Vector2i pixelPos = sf::Mouse::getPosition(window);
+    Vector2f worldPos = window.mapPixelToCoords(pixelPos);
+    dir = atan( (worldPos.y - pos.y)/(worldPos.x - pos.x) );
+
+
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
     {
         pos.x --;
@@ -30,13 +38,28 @@ void Player::frame()
     {
         pos.y ++;
     }
-    
+
 }
 
 
 void Player::draw()
 {
-    
+  int s_pos_x = 0;
+  int s_pos_y = 0;
+  if(dir<-0.79 && dir>-2.35)
+    s_pos_y = 0;
+  else if(dir<0.79 && dir>-0.79)
+    s_pos_y = 27;
+  else if(dir<0.79 && dir<-2.35)
+    s_pos_y = 54;
+  else
+    s_pos_y = 81;
+
+
+  sprite.setPosition(float(pos.x), float(pos.y));
+
+  sprite.setTextureRect( IntRect(s_pos_x, s_pos_y, 15, 27));
+  g_window.draw(sprite);
 }
 
 bool Player::mustRemove() const
--- player.h
+++ player.h
@@ -2,12 +2,16 @@
 #define PLAYER_H
 
 #include "entity.h"
+#include <math.h>
 
 class Player : public Entity{
 public:
     Player();
     ~Player();
     bool mustRemove() const;
+
+    int anim_step;
+    float dir;
 
     void draw();
     void frame();
